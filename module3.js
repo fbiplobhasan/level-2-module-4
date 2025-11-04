01ab138fc8aa4040c2a2551ec63caae9006aede5
Biplob Hasan <fbiplobhasan2024@gmail.com>
2025-11-04 21:39:41 +0600

    Linked List Implementation - prepend()

--- module3.js
+++ module3.js
@@ -190,12 +190,12 @@
 
 //     append(value) {
 //         const newNode = new Node(value);
-        // if the linked list is empty
+// if the linked list is empty
 //         if (this.head === null) {
 //             this.head = newNode;
 //             this.tail = newNode;
 //         } else {
-            // if the linked list is not empty
+// if the linked list is not empty
 //             this.tail.next = newNode;
 //             this.tail = newNode;
 //         }
@@ -222,3 +222,97 @@
 // linkedList.append(3)
 
 // linkedList.print()
+
+// Video (8)
+// Linked List Implementation - prepend()
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    append(value) {
+        const newNode = new Node(value);
+        // if the linked list is empty
+        if (this.head === null) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            // if the linked list is not empty
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+    }
+    prepend(value) {
+        const newNode = new Node(value);
+        // if the linked list is empty
+        if (this.head === null) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            // if the linked list is not empty
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.length++;
+    }
+    insert(index, value) {
+        if (index < 0 || index > this.length) {
+            console.log("Index out of bound:");
+            return undefined
+        }
+        // if the insert is in the start of the linked list
+        if (index === 0) {
+            return this.prepend(value)
+        }
+        // if the insert is in the end of the list
+        if (index === this.length) {
+            return this.append(value)
+        }
+
+        // if the insert is in the middle
+        // find the leading node
+        let count = 0;
+        let leadingNode = this.head;
+        while (count !== index - 1) {
+            leadingNode = leadingNode.next;
+            count++;
+        }
+        console.log(leadingNode);
+    }
+    remove() { }
+    print() {
+        const arr = [];
+
+        let currentNode = this.head;
+
+        while (currentNode !== null) {
+            arr.push(currentNode.value);
+            currentNode = currentNode.next;
+        }
+        console.log(arr.join(" ---> "), " ---> null ");
+    }
+}
+
+const linkedList = new LinkedList();
+
+linkedList.append(1)
+linkedList.append(2)
+linkedList.append(3)
+
+linkedList.prepend(10)
+linkedList.prepend(20)
+linkedList.prepend(30)
+
+linkedList.insert(2, 100)
+
+linkedList.print()
