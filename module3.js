dc6d0d134418aa9ccfb3953805f6da3262f0ba82
Biplob Hasan <fbiplobhasan2024@gmail.com>
2025-11-05 06:45:47 +0600

    something new

--- module3.js
+++ module3.js
@@ -241,12 +241,12 @@
 
 //     append(value) {
 //         const newNode = new Node(value);
-        // if the linked list is empty
+// if the linked list is empty
 //         if (this.head === null) {
 //             this.head = newNode;
 //             this.tail = newNode;
 //         } else {
-            // if the linked list is not empty
+// if the linked list is not empty
 //             this.tail.next = newNode;
 //             this.tail = newNode;
 //         }
@@ -255,12 +255,12 @@
 //     }
 //     prepend(value) {
 //         const newNode = new Node(value);
-        // if the linked list is empty
+// if the linked list is empty
 //         if (this.head === null) {
 //             this.head = newNode;
 //             this.tail = newNode;
 //         } else {
-            // if the linked list is not empty
+// if the linked list is not empty
 //             newNode.next = this.head;
 //             this.head = newNode;
 //         }
@@ -272,17 +272,17 @@
 //             console.log("Index out of bound:");
 //             return undefined
 //         }
-        // if the insert is in the start of the linked list
+// if the insert is in the start of the linked list
 //         if (index === 0) {
 //             return this.prepend(value)
 //         }
-        // if the insert is in the end of the list
+// if the insert is in the end of the list
 //         if (index === this.length) {
 //             return this.append(value)
 //         }
 
-        // if the insert is in the middle
-        // find the leading node
+// if the insert is in the middle
+// find the leading node
 //         const leadingNode = this._traverseToIndex(index - 1);
 //         const holdingNode = leadingNode.next;
 
@@ -295,7 +295,7 @@
 
 //     }
 //     remove() { }
-    // private helper method
+// private helper method
 //     _traverseToIndex(index) {
 //         let count = 0;
 //         let currentNode = this.head;
@@ -329,3 +329,138 @@
 // linkedList.insert(2, 100)
 
 // linkedList.print()
+
+// Video (10)
+// Linked List Implementation - remove()
+
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    append(value) {
+        const newNode = new Node(value);
+        // if the linked list is empty
+        if (this.head === null) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            // if the linked list is not empty
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+    prepend(value) {
+        const newNode = new Node(value);
+        // if the linked list is empty
+        if (this.head === null) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            // if the linked list is not empty
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.length++;
+        return this;
+    }
+    insert(index, value) {
+        if (index < 0 || index > this.length) {
+            console.log("Index out of bound:");
+            return undefined
+        }
+        // if the insert is in the start of the linked list
+        if (index === 0) {
+            return this.prepend(value)
+        }
+        // if the insert is in the end of the list
+        if (index === this.length) {
+            return this.append(value)
+        }
+
+        // if the insert is in the middle
+        // find the leading node
+        const leadingNode = this._traverseToIndex(index - 1);
+        const holdingNode = leadingNode.next;
+
+        const newNode = new Node(value)
+
+        leadingNode.next = newNode;
+        newNode.next = holdingNode;
+
+        this.length++;
+
+    }
+    remove(index) {
+        if (index === 0) {
+            const removedItem = this.head.value;
+            this.head = this.head.next;
+
+            if (this.length === 1) {
+                this.tail = null;
+            }
+            this.length--;
+            return removedItem;
+        }
+
+        const leadingNode = this._traverseToIndex(index - 1)
+        const nodeToRemove = leadingNode.next;
+
+        leadingNode.next = nodeToRemove.next;
+
+        if (leadingNode.next === null) {
+            this.tail = leadingNode;
+        }
+        return nodeToRemove.value;
+    }
+    // private helper method
+    _traverseToIndex(index) {
+        let count = 0;
+        let currentNode = this.head;
+        while (count !== index) {
+            currentNode = currentNode.next;
+            count++;
+        }
+        return currentNode
+    }
+    print() {
+        const arr = [];
+
+        let currentNode = this.head;
+
+        while (currentNode !== null) {
+            arr.push(currentNode.value);
+            currentNode = currentNode.next;
+        }
+        console.log(arr.join(" ---> "), " ---> null ");
+    }
+}
+
+const linkedList = new LinkedList();
+
+linkedList.append("A")
+// linkedList.append("B")
+// linkedList.append("C")
+// linkedList.append("D")
+
+// linkedList.prepend(10)
+// linkedList.prepend(20)
+// linkedList.prepend(30)
+
+linkedList.print()
+// linkedList.insert(2, 100)
+// linkedList.remove(2)
+linkedList.remove(0)
+
+linkedList.print()
