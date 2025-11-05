f60eb30e134315b22f2a0ed4c9f5bc45e82245ef
Biplob Hasan <fbiplobhasan2024@gmail.com>
2025-11-05 06:50:45 +0600

    first commit

---
+++ yt-explore.js
@@ -0,0 +1,22 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.naxt = null;
+    }
+}
+
+class Queue {
+    constructor() {
+        this.first = null
+        this.last = null
+        this.length = 0
+    }
+    enqueue() {
+
+    }
+    dequeue() {
+
+    }
+}
+
+let myQueue = new Queue();
